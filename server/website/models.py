552803ce93ac03053b49c26cbc680e1456176eee
Newt Yao <pomelo810205@gmail.com>
2024-06-23 14:55:18 +0800

    done with register and login except social login

--- server/website/models.py
+++ server/website/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    email = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=20)
+    SKI_EXPERIENCE={
+        "FT": "First Time",
+        "GZ": "Green Zone",
+        "BZ": "Blue Zone",
+        "EXZ": "Black Zone",
+    }
+    level = models.CharField(max_length=5, choices=SKI_EXPERIENCE, default="FT")
+
+    def __str__(self) -> str:
+        return (f"{self.name}")
+    
+class Ticket(models.Model):
+    pass
+    
